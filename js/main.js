580477636dfdeefe5b347c43d1f2de984ed2871d
Mandar A <mandarashtekar@users.noreply.github.com>
2020-07-26 14:51:45 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -275,7 +275,7 @@ async function perform(net) {
 function drawBody(personSegmentation) {
     console.log("Virtual Background effect");
 
-    ctx.drawImage(selfvideo, 0, 0, selfvideo.width, selfvideo.height);
+    // ctx.drawImage(selfvideo, 0, 0, selfvideo.width, selfvideo.height);
     var imageData = ctx.getImageData(0,0, selfvideo.width, selfvideo.height);
     var pixel = imageData.data;
     for (var p = 0; p<pixel.length; p+=4)
@@ -290,8 +290,9 @@ function drawBody(personSegmentation) {
     var image = new Image();
     image.src = "./images/sphinx.jpg";
     ctx.drawImage(image, 0, 0, selfvideo.width, selfvideo.height);
+    ctx.drawImage(selfvideo, 0, 0, selfvideo.width, selfvideo.height);
 
-    ctx.globalAlpha = 0.5;
+    // ctx.globalAlpha = 0.5;
 }
 
 /*function drawBody(personSegmentation) {
